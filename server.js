929c84dc80bbd5e2071d603b1970d6cbc9e5e38e
BG-dev <nik.azizoff2016@gmail.com>
2022-03-22 17:52:30 +0300

    create start server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server was started on port: ${port}`)
+})
